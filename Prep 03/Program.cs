38b2017f47420841b1b2204515035adf18bb2f99
wesleyfigueira <wesleyfigueira1993@gmail.com>
2024-02-27 17:13:11 -0300

    prep 03 finished

---
+++ Prep 03/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int magicNumber;
+        int guessNumber;
+
+
+            Console.WriteLine("What is the magic Number ?");
+            string userInput = Console.ReadLine();
+            magicNumber = int.Parse(userInput);
+
+        do
+        {
+            Console.WriteLine("What is your guess ?");
+            string userInput2 = Console.ReadLine();
+            guessNumber = int.Parse(userInput2);
+
+            if (magicNumber < guessNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber > guessNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+
+        } while (magicNumber != guessNumber);
+
+        Console.WriteLine("Congratulations! You guessed the magic number!");
+    }
+}
